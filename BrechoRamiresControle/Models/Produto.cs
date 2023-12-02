b9f7d187827d36b9ff2a06c47a509bf48d9846b0
Tales Ribeiro Oliveira <talesr.oliveira@icloud.com>
2023-12-02 12:20:03 -0300

    Adicionar arquivos de projeto.

---
+++ BrechoRamiresControle/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace BrechoRamiresControle.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public string Marca { get; set; }
+        public string Codigo { get; set; }
+        public string Categoria { get; set; }
+        public string Cor { get; set; }
+        public string Tamanho { get; set; }
+        public decimal PrecoPago { get; set; }
+        public decimal Preco { get; set; }
+        public int Origem { get; set; }
+
+    }
+}
