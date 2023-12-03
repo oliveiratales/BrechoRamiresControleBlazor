c78fb9313eb26947e6b58318af6d0a4a4fa86b42
Tales Ribeiro Oliveira <talesr.oliveira@icloud.com>
2023-12-03 10:19:11 -0300

    Finalizando seção de vendas, indicadores e configurações

--- BrechoRamiresControle/Models/Produto.cs
+++ BrechoRamiresControle/Models/Produto.cs
@@ -1,11 +1,15 @@
-﻿namespace BrechoRamiresControle.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrechoRamiresControle.Models
 {
     public class Produto
     {
+        [Key]
+        public string Codigo { get; set; }
+
         public string Nome { get; set; }
         public int Quantidade { get; set; }
-        public string Marca { get; set; }
-        public string Codigo { get; set; }
+        public string Marca { get; set; }     
         public string Categoria { get; set; }
         public string Cor { get; set; }
         public string Tamanho { get; set; }
---
+++ BrechoRamiresControle/Models/Venda.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrechoRamiresControle.Models
+{
+    public class Venda
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public DateTime DataVenda { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Desconto { get; set; }
+        public string FormaDePagamento { get; set; }
+        public string Cliente { get; set; }
+        public int Vendedor { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
